c5d296a04831a7b1f82ec0f07343ae6e2ccbb7c1
luke donley <titustwimbly@lukes-MacBook-Pro.local>
2021-03-03 20:27:39 -0600

    configure backend user authentication, refactoring

--- base/views/product_views.py
+++ base/views/product_views.py
@@ -1,18 +1,13 @@
 from django.shortcuts import render
-from django.http import JsonResponse
-from rest_framework.decorators import api_view
+
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.permissions import IsAuthenticated, IsAdminUser
 from rest_framework.response import Response
 
-from .models import Product
-from .serializers import ProductSerializer
+from base.models import Product
+from base.serializers import ProductSerializer
 
-# Create your views here.
-
-
-@api_view(['GET'])
-def getRoute(request):
-    routes = []
-    return Response(routes)
+from rest_framework import status
 
 
 @api_view(['GET'])
